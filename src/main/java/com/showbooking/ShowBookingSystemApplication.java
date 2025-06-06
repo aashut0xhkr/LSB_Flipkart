5d8880076bbc93dae09bb78ea0ea13ee34c4f497
Ashutosh <aashut0xh@gmail.com>
2025-06-06 22:39:58 +0530

    Commit

--- src/main/java/com/showbooking/ShowBookingSystemApplication.java
+++ src/main/java/com/showbooking/ShowBookingSystemApplication.java
@@ -11,30 +11,30 @@ import java.util.Map;
 public class ShowBookingSystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShowBookingSystemApplication.class, args);
-        BookingService service = new BookingService();
-
-        service.registerShow("TMKOC", "Comedy");
-
-        Map<String, Integer> slots = new HashMap<>();
-        slots.put("9:00-10:00", 3);
-        slots.put("12:00-13:00", 2);
-        slots.put("15:00-16:00", 5);
-
-        service.onboardShowSlots("TMKOC", slots);
-
-        service.showAvailByGenre("Comedy");
-
-        service.bookTicket("UserA", "TMKOC", "12:00-13:00", 2);
-
-        service.showAvailByGenre("Comedy");
-
-        service.viewBookings("UserA");
-
-        service.cancelBooking("1000");
-
-        service.showAvailByGenre("Comedy");
-
-        service.trendingShow();
+//        BookingService service = new BookingService();
+//
+//        service.registerShow("TMKOC", "Comedy");
+//
+//        Map<String, Integer> slots = new HashMap<>();
+//        slots.put("9:00-10:00", 3);
+//        slots.put("12:00-13:00", 2);
+//        slots.put("15:00-16:00", 5);
+//
+//        service.onboardShowSlots("TMKOC", slots);
+//
+//        service.showAvailByGenre("Comedy");
+//
+//        service.bookTicket("UserA", "TMKOC", "12:00-13:00", 2);
+//
+//        service.showAvailByGenre("Comedy");
+//
+//        service.viewBookings("UserA");
+//
+//        service.cancelBooking("1000");
+//
+//        service.showAvailByGenre("Comedy");
+//
+//        service.trendingShow();
 
 
     }
